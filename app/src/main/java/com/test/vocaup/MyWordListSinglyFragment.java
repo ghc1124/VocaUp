42b0e8b6b430ff68dec29c0ae93cf89473e9e3ab
gh9802 <wjdrkdgns125@naver.com>
2021-01-23 17:55:26 +0900

    DB 손 보기 전

--- app/src/main/java/com/test/vocaup/MyWordListSinglyFragment.java
+++ app/src/main/java/com/test/vocaup/MyWordListSinglyFragment.java
@@ -4,10 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class MyWordListSinglyFragment extends Fragment {
+
+    private ImageView image_word;
+    private Button btn_pron;
+    private Button btn_back;
+    private Button btn_next;
+    private TextView text_mean;
+    private TextView text_s_mean;
+    private TextView text_sentence;
+
     public static MyWordListSinglyFragment newInstance() { // 모든 프래그먼트에 공통으로 들어가야될 부분!!
         return new MyWordListSinglyFragment();
     }
--- app/src/main/java/com/test/vocaup/StudyFragment.java
+++ app/src/main/java/com/test/vocaup/StudyFragment.java
@@ -4,10 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class StudyFragment extends Fragment {
+
+    private TextView text_level;
+    private Button btn_w_to_m;
+    private Button btn_m_to_w;
+    private Button btn_match;
+    private Button btn_p_to_m;
+    private Button btn_arrange;
+    private Button btn_fill_blank;
+    private Button btn_back;
+
     public static StudyFragment newInstance() { // 모든 프래그먼트에 공통으로 들어가야될 부분!!
         return new StudyFragment();
     }
