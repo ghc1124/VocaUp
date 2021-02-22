9ec2a257b2d469d8f4e3cde3f1eaa4db59f0c698
kirin316 <ghc0409@gmail.com>
2021-02-22 23:32:50 +0900

    20210225

--- app/src/main/java/com/test/vocaup/activity/DetailActivity.java
+++ app/src/main/java/com/test/vocaup/activity/DetailActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,6 +31,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_detail);
 
         Intent intent = getIntent();
