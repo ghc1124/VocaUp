4a4b861090e70140d4374f2b7ebd0cb4107b41fe
kirin316 <ghc0409@gmail.com>
2021-03-25 21:33:15 +0900

    20210325 중간 결산4 -> 버튼 크기 바뀌는 문제 수정

--- app/src/main/java/com/test/vocaup/fragment/MenuFragment.java
+++ app/src/main/java/com/test/vocaup/fragment/MenuFragment.java
@@ -86,11 +86,11 @@ public class MenuFragment extends Fragment  {
 
                         testManager.setRecap(5);
                         testManager.setBlank_spelling(1);
-                        testManager.setMean_spelling(1);
+                        testManager.setMean_spelling(0);
                         testManager.setSpelling_mean(1);
                         testManager.setSpelling_mean_link(1);
                         testManager.setSpelling_sort(1);
-                        testManager.setPron_mean(0);
+                        testManager.setPron_mean(1);
 
                         Manager resultManager = connect_put.changeUserInfo(testManager);
                         ((MenuActivity)getActivity()).manager = ((MenuActivity)getActivity()).getManager();
